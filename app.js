b4d0cebfbaa230296ae4d401b63396d052e09d3e
bwapes <benwaples@gmail.com>
2020-07-08 15:20:32 -0700

    buttons are working and we are getting the correct value for each

--- app.js
+++ app.js
@@ -2,10 +2,21 @@
 import { getRandomThrow, checkResult } from './gameUtils.js';
 
 const startGame = document.querySelector('.startGame');
+const theGame = document.querySelector('.theGame');
+const shootButton = document.querySelector('.shootButton');
+
 
 startGame.addEventListener('click', () => {
-    console.log('working')
+    startGame.classList.add('hidden');
+    theGame.classList.remove('hidden');
 })
+
+shootButton.addEventListener('click', () => {
+    const checkRadio = document.querySelector('input:checked');
+    const checkRadioValue = checkRadio.value;
+    console.log(checkRadioValue)
+})
+
 // initialize state
 
 // set event listeners to update state and DOM 
