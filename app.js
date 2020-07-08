91b3711d6c9bf23c0337ed0ad1ee6d15a1685a53
bwapes <benwaples@gmail.com>
2020-07-08 15:11:51 -0700

    we got the start button

--- app.js
+++ app.js
@@ -1,5 +1,11 @@
 // import functions and grab DOM elements
+import { getRandomThrow, checkResult } from './gameUtils.js';
 
+const startGame = document.querySelector('.startGame');
+
+startGame.addEventListener('click', () => {
+    console.log('working')
+})
 // initialize state
 
 // set event listeners to update state and DOM 
